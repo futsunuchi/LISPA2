618834de779ea9f9bf207f3d94d4d35c1e27fa1d
Juan Sebastian Hoyos <juanse00713@hotmail.com>
2019-06-04 10:53:19 -0400

    Finished question 2

--- A2.lisp
+++ A2.lisp
@@ -1,5 +1,5 @@
-; STUDENT					ID	
-; Juan Sebastian Hoyos	40087920
+; STUDENT							ID	
+; Juan Sebastian Hoyos			40087920
 ; Noah Freger
 ; Abdalla Osman
 ; Kevin Rao 
@@ -13,3 +13,12 @@
 ;e) (P O L)
 ;f) ((CAR A) (CONS A B))
 
+;Question 2
+;a)
+(cadddr '(A B C D))
+;b)
+(car (cdadar '((A (B C)) E)) ) ;I'm not sure if we are aloud to use different format than what he gave
+;c)
+(caaar '(((D) E) U))
+;d)
+(cadar '(((D) E) U))
