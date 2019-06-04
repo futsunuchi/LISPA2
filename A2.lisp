7db3949a8a563baeb726d5fb112610ffbd7aee58
Juan Sebastian Hoyos <juanse00713@hotmail.com>
2019-06-04 10:56:00 -0400

    Fixed the format

--- A2.lisp
+++ A2.lisp
@@ -5,20 +5,19 @@
 ; Kevin Rao 
 
 ;Assignment 2:
-;Question 1
-;a) (A (B C))
-;b) (D (E F))
-;c) ( (B C) )
-;d) (E F)
-;e) (P O L)
-;f) ((CAR A) (CONS A B))
+;Exercise #1
+;Part A
+	;a) (A (B C))
+	;b) (D (E F))
+	;c) ( (B C) )
+	;d) (E F)
+	;e) (P O L)
+	;f) ((CAR A) (CONS A B))
 
-;Question 2
-;a)
-(cadddr '(A B C D))
-;b)
-(car (cdadar '((A (B C)) E)) ) ;I'm not sure if we are aloud to use different format than what he gave
-;c)
-(caaar '(((D) E) U))
-;d)
-(cadar '(((D) E) U))
+;Part B
+	;a) (cadddr '(A B C D))
+	;b) (car (cdadar '((A (B C)) E)) ) I'm not sure if we are aloud to use different format than what he gave
+	;c) (caaar '(((D) E) U))
+	;d)	(cadar '(((D) E) U))
+	
+;Exercise #2
